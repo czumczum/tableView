63d701008ac6a1a73f29613be9d4e433714010dc
Ula <czumczum@users.noreply.github.com>
2018-08-01 13:13:02 -0400

    custom cell

---
+++ kjsdhjhsdhjdsjds/CustomCell/TrackDetailCell.swift
@@ -0,0 +1,12 @@
+
+
+import UIKit
+
+class TrackDetailCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+    }
+
+}
